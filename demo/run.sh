1b574ccf1964143f9b51035d3e4eb85fc2f8f209
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2024-05-09 12:01:54 +0100

    add a minimal demo

---
+++ demo/run.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+set -euxo pipefail
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+export WASMTIME_NEW_CLI=0 
+
+${SCRIPT_DIR}/chicory ${SCRIPT_DIR}/target/wasm32-unknown-unknown/release/add.wasm --invoke add 2 3
+wasmtime ${SCRIPT_DIR}/target/wasm32-unknown-unknown/release/add.wasm --invoke add 2 3
