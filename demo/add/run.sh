87758cb0e73f047a1cf5bb912c4607f48db78e44
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2024-05-13 12:00:03 +0100

    add demos

--- demo/add/run.sh
+++ demo/add/run.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-export WASMTIME_NEW_CLI=0 
+export WASMTIME_NEW_CLI=0
 
-${SCRIPT_DIR}/chicory ${SCRIPT_DIR}/target/wasm32-unknown-unknown/release/add.wasm --invoke add 2 3
+${SCRIPT_DIR}/../chicory ${SCRIPT_DIR}/target/wasm32-unknown-unknown/release/add.wasm --invoke add 2 3
 wasmtime ${SCRIPT_DIR}/target/wasm32-unknown-unknown/release/add.wasm --invoke add 2 3
